b622081d5d19c6444860073ababb45b7ec82dce8
jhelie <jean.helie@gmail.com>
2015-05-19 16:07:23 +0100

    0.0.1

--- boltzmann_inversion.py
+++ boltzmann_inversion.py
@@ -24,13 +24,15 @@ git: https://github.com/jhelie/boltzmann_inversion
 This script the opposite of the log of densities. The densities in each column of the input
 file are normalised with respect to themselves.
 
+NB: for quick plotting with xmgrace first change NaN values in output file to 0.
+
 [ USAGE ]
 
 Option	      Default  	Description                    
 -----------------------------------------------------
 -f			: xvg file(s)
 -o			: name of outptut file
--col	3	: ignore the first X columns in the input file
+-col		1	: ignore the first X column(s) in the input file
 --comments	@,#	: lines starting with these characters will be considered as comment
 
 Other options
@@ -43,7 +45,7 @@ Other options
 #options
 parser.add_argument('-f', nargs=1, dest='xvgfilename', help=argparse.SUPPRESS, required=True)
 parser.add_argument('-o', nargs=1, dest='output_file', default=["no"], help=argparse.SUPPRESS)
-parser.add_argument('--col', nargs=1, dest='col_start', default=[3], type=int, help=argparse.SUPPRESS)
+parser.add_argument('--col', nargs=1, dest='col_start', default=[1], type=int, help=argparse.SUPPRESS)
 parser.add_argument('--comments', nargs=1, dest='comments', default=['@,#'], help=argparse.SUPPRESS)
 
 #other options
@@ -98,6 +100,8 @@ if not os.path.isfile(args.xvgfilename):
 	sys.exit(1)
 if args.output_file == "no":
 	args.output_file = args.xvgfilename[:-4].split('/')[-1] + '_bi.xvg'
+elif args.output_file[-3:] != 'xvg':
+	args.output_file += '.xvg'
 
 ##########################################################################################
 # FUNCTIONS DEFINITIONS
@@ -105,11 +109,9 @@ if args.output_file == "no":
 
 def load_xvg():
 	
-	global y_min
-	global y_max
-	global nb_rows
-	global nb_cols
-	global distances
+	global x_axis
+	global s_species
+	global nb_species
 	global data_energy
 			
 	#get file content
@@ -118,46 +120,39 @@ def load_xvg():
 		lines = f.readlines()
 	
 	#determine legends and nb of lines to skip
+	s_species = []
 	tmp_nb_rows_to_skip = 0
 	for l_index in range(0,len(lines)):
 		line = lines[l_index]
 		if line[0] in args.comments:
 			tmp_nb_rows_to_skip += 1
+			if len(line.split("legend \"")) > 1:
+				s_species.append(line.split("legend \"")[-1][:-2])
 	
 	#get data
-	tmp_data = np.loadtxt(filename, skiprows = tmp_nb_rows_to_skip)
-		
-	#store data
-	distances = tmp_data[:,0]
-	if args.cholesterol:
-		data_energy = tmp_data[:,1]
-	else:
-		data_energy = tmp_data[:,4]
-		
-	#transform by taking opposite of log
-	data_energy = -np.log(data_energy)
-	
-	#translate curve so that its minimum is zero
-	y_min = np.nanmin(data_energy)
-	data_energy -= y_min			
-	y_max = 12
+	data_density = np.loadtxt(filename, skiprows = tmp_nb_rows_to_skip)
+	x_axis = np.copy(data_density[:,0])
+	data_density = data_density[:,args.col_start:]
+	s_species = s_species[args.col_start-1:]
+	nb_species = np.shape(data_density)[1]
+	data_density /= np.sum(data_density, axis = 0)
+	data_energy = -np.log(data_density)
+	data_energy -= np.min(data_energy, axis = 0)
+	data_energy[np.isinf(data_energy)] = np.nan
 	
 	return
 def write_xvg():
 	#open files
-	filename_xvg = os.getcwd() + '/' + str(args.output_file) + '.xvg'
+	filename_xvg = os.getcwd() + '/' + str(args.output_file)
 	output_xvg = open(filename_xvg, 'w')
 	
 	#general header
-	output_xvg.write("# [average xvg - written by botlzmann_inversion v" + str(version_nb) + "]\n")
-	output_xvg.write("# - file: " + str(args.xvgfilename) + "\n")	
+	output_xvg.write("# [free energy estimation - written by botlzmann_inversion v" + str(version_nb) + "]\n")
+	output_xvg.write("# input file: " + str(args.xvgfilename) + "\n")	
 	#xvg metadata
-	output_xvg.write("@ title \"Average xvg\"\n")
+	output_xvg.write("@ title \"Relative free energies\"\n")
 	output_xvg.write("@ xaxis label \"z distance to bilayer center (Angstrom)\"\n")
-	if args.cholesterol:
-		output_xvg.write("@ yaxis label \"relative free energy of CHOL (kT)\"\n")
-	else:
-		output_xvg.write("@ yaxis label \"relative free energy of POPS (kT)\"\n")
+	output_xvg.write("@ yaxis label \"relative free energies (kT)\"\n")
 	output_xvg.write("@ autoscale ONREAD xaxes\n")
 	output_xvg.write("@ TYPE XY\n")
 	output_xvg.write("@ view 0.15, 0.15, 0.95, 0.85\n")
@@ -165,15 +160,16 @@ def write_xvg():
 	output_xvg.write("@ legend box on\n")
 	output_xvg.write("@ legend loctype view\n")
 	output_xvg.write("@ legend 0.98, 0.8\n")
-	output_xvg.write("@ legend length 3\n")
-	output_xvg.write("@ s0 legend \"free energy\"\n")
-	
-	#replace inf by nan (since they're not plotted)
-	data_energy[np.isinf(data_energy)] = np.nan
-	
+	output_xvg.write("@ legend length " + str(nb_species) + "\n")
+	for s_index in range(0, nb_species):
+		#debug
+		print str(s_species[s_index])
+		output_xvg.write("@ s" + str(s_index) + " legend \"" + str(s_species[s_index]) + "\"\n")
 	#data
-	for r in range(0, np.shape(data_energy)[0]):
-		results = str(distances[r]) + "	" + str(data_energy[r])
+	for x_index in range(0, np.shape(data_energy)[0]):
+		results = str(x_axis[x_index])
+		for s_index in range(0, nb_species):
+			results += "	" + str(data_energy[x_index, s_index])
 		output_xvg.write(results + "\n")		
 	output_xvg.close()	
 
@@ -189,6 +185,6 @@ write_xvg()
 #=========================================================================================
 # exit
 #=========================================================================================
-print "\nFinished successfully! Check result in file '" + str(args.output_file) + ".xvg'."
+print "\nFinished successfully! Check result in file '" + str(args.output_file)
 print ""
 sys.exit(0)
